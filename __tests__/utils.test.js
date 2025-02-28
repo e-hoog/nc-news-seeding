4397fc86b084e6c0b918cf5e1408f961c48e06e5
e-hoog <eddiehoog@hotmail.com>
2025-02-28 14:12:00 +0000

    add util tests for createLookupObject function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,6 @@
 const {
-  convertTimestampToDate
+  convertTimestampToDate,
+  createLookupObject
 } = require("../db/seeds/utils");
 
 describe("convertTimestampToDate", () => {
@@ -38,3 +39,84 @@ describe("convertTimestampToDate", () => {
   });
 });
 
+describe('createLookupObject', () => {
+  test('returns a new object', () => {
+    const rowInput = [{
+      key : "test",
+      value: "passed"
+    }]
+    const keyInput = "key"
+    const valueInput = "value"
+    const result = createLookupObject(rowInput, keyInput, valueInput)
+    expect(result).toBeObject()
+  });
+  test('creates a lookup object with 1 correct key value pair if given a single object in the database', () => {
+    const rowInput = [{
+      key : "test",
+      value: "passed",
+    }]
+    const keyInput = "key"
+    const valueInput = "value"
+    const result = createLookupObject(rowInput, keyInput, valueInput)
+    const expected = {test: "passed"}
+    expect(result).toEqual(expected)
+  })
+  test('creates a lookup object with multiple correct key value pairs if given multiple objects in the database', () => {
+    const rowInput = [{
+      key : "test",
+      value: "passed",
+    },
+    {
+      key : "multiple test",
+      value: "passed",
+    }
+  ]
+    const keyInput = "key"
+    const valueInput = "value"
+    const result = createLookupObject(rowInput, keyInput, valueInput)
+    const expected = {test: "passed", "multiple test" : "passed"}
+    expect(result).toEqual(expected)
+  });
+  test('ignores all other key value pairs in the database', () => {
+    const rowInput = [{
+      key : "test",
+      value: "passed",
+      wrongKey: "wrongValue"
+    },
+    {
+      key : "multiple test",
+      value: "passed",
+      wrongKey: "wrongValue2"
+    }
+  ]
+    const keyInput = "key"
+    const valueInput = "value"
+    const result = createLookupObject(rowInput, keyInput, valueInput)
+    const expected = {test: "passed", "multiple test" : "passed"}
+    expect(result).toEqual(expected)
+  });
+  test('does not mutate the input', () => {
+    const rowInput = [{
+      key : "test",
+      value: "passed",
+    }]
+    const keyInput = "key"
+    const valueInput = "value"
+    createLookupObject(rowInput, keyInput, valueInput)
+    const control = [{
+      key : "test",
+      value: "passed",
+    }]
+    expect(rowInput).toEqual(control)
+  })
+  test('returns empty object if given a database without correct key value pairs', () => {
+    const rowInput = [{
+      key1 : "value1",
+      key2 : "value2",
+    }]
+    const keyInput = "otherKey"
+    const valueInput = "otherValue"
+    const result = createLookupObject(rowInput, keyInput, valueInput)
+    expect(result).toEqual({})
+  });
+});
